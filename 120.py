743f083e1f48865bf2278f26c8e6cc17d340e81e
Guilherme Ruiz do Nascimento <60757792+L0wZZaRD@users.noreply.github.com>
2020-03-04 17:47:49 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,14 @@
+# autor: 1DSI3 @Guilherme Ruiz
+# feito em 03/03/2020
+
+#inicio_algoritmo
+
+#var
+x = 0      					#Declaração do contador
+valor = 2  					#Declarção da Variavel valor
+while (x <= 21):			#Estrutura de repetição
+	valor = valor * valor	#Multiplicação de variavel valor
+	x = x + 1				#Acrescido de 1 ao contador
+print(valor) 				#Exibe o valor para o usuário
+
+#fim_algoritmo
